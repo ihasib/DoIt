cca541ec9ac4311c95872d162f45bc87017138e8
S. M. Hasibur Rahman <hasibur.r@samsung.com>
2024-10-22 17:26:42 +0600

    Project initiated

---
+++ app/src/main/java/com/example/todolist/Model/ToDoModel.java
@@ -0,0 +1,22 @@
+package com.example.todolist.Model;
+
+public class ToDoModel {
+    Integer id;
+    String task;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+}
